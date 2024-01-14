317d3f9faf38fec54ab31deb4ea07b987093a6e5
RedDeeer <155503838+RedDeeer@users.noreply.github.com>
2024-01-14 18:31:17 +0000

    Update FR.java

--- FR.java
+++ FR.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     Integer myInt = 512;
-    Double myDouble = 5.8;
+    Double myDouble = 5;
     Character myChar = 'BPPS';
     System.out.println(myInt);
     System.out.println(myDouble);
