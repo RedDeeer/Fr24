d47b9201f5f9dcd1165a4c59e96d2ff1a4bcce6b
RedDeeer <155503838+RedDeeer@users.noreply.github.com>
2024-02-27 17:12:43 +0000

    Update FR.java

--- FR.java
+++ FR.java
@@ -12,7 +12,7 @@ public class Main {
 public class Main {
   public static void main(String[] args) {
     Integer myInt = 5;
-    Double myDouble = 599;
+    Double myDouble = 59;
     Character myChar = 'A';
     System.out.println(myInt.intValue());
     System.out.println(myDouble.doubleValue());
