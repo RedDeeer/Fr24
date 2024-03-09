04373c78b42c37a9f9eb58396d26914ea6372361
RedDeeer <155503838+RedDeeer@users.noreply.github.com>
2024-03-09 18:13:07 +0000

    Update FR.java

--- FR.java
+++ FR.java
@@ -4,7 +4,7 @@ public class Main {
     Double Doble = 6;
     Character myhar = 'Bl';
     System.ot.priln(mydt);
-    System.out.prinln(Doble);
+    System.out.prinn(Doble);
     System.out.println(myChar);
   }
 }
